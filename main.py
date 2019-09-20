1d6f98030bc19aa138e75fb596e7662c116f67ac
hanki <hankilapio@gmail.com>
2019-09-20 06:20:06 +0300

    Started parsing the site with BeautifulSoup

---
+++ main.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+searchpage_url = r'http://audiobookbay.nl/?s='
+# TODO: Use input() so you can decide what to search
+# TODO: string converter (html encrypt), e.g. "Harry Potter" -> "harry+potter"
+user_search = "harry+potter+collection"
+
+url = searchpage_url + user_search
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+
+contents = soup.select('#content .post')
+
+for post in contents:
+    name = post.select_one('.postTitle h2 a').text.strip()
+    # TODO: Parse book language for postInfo
+
+    print(name)
