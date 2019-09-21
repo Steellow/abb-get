2220fdf8d42ecc25f25542d2a0ae744aa2fd394e
Steellow <hankilapio@gmail.com>
2019-09-21 11:41:15 +0300

    Put the code in main method and fixed the bug where soup only selected 1 post

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 
-searchpage_url = r'http://audiobookbay.nl/?s='
-# TODO: Use input() so you can decide what to search
-# TODO: string converter (html encrypt), e.g. "Harry Potter" -> "harry+potter"
-user_search = "harry+potter+collection"
 
-url = searchpage_url + user_search
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
+def main():
+    searchpage_url = r'http://audiobookbay.nl/?s='
+    # TODO: Use input() so you can decide what to search
+    # TODO: string converter (html encrypt), e.g. "Harry Potter" -> "harry+potter"
+    user_search = "harry+potter+collection"
 
-contents = soup.select('#content .post')
+    url = searchpage_url + user_search
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-for post in contents:
-    name = post.select_one('.postTitle h2 a').text.strip()
-    # TODO: Parse book language for postInfo
+    contents = soup.select('.post')
 
-    print(name)
+    print(len(contents))
+
+
+    for post in contents:
+        name = post.select_one('.postTitle h2 a').text.strip()
+        # TODO: Parse book language for postInfo
+
+        print(name)
+
+
+main()
